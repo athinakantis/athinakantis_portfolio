010fc08b46f3e3d927ae7cf818b4e3cb43107264
Athina <athina.kantis@gmail.com>
2025-02-12 20:09:16 +0200

    remove header from about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,12 @@
 export const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <>
+            <main>
+                <section id="about" className='content-section'>
+                    <img id='portrait' src="/portrait_athina.jpg" alt="" />
+                    <img id='old_drawing' src="/old_drawing_700w.png" alt="Drawn illustration made by a young Athina portraying a girl by a house on a cloudy day" />
+                </section>
+            </main>
+        </>
     )
 }
