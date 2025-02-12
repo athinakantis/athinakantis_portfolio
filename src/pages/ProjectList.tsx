9bb565773f789a85d13150b14fe88f200da01d04
Athina <athina.kantis@gmail.com>
2025-02-12 20:10:21 +0200

    remove header from projectlist

--- src/pages/ProjectList.tsx
+++ src/pages/ProjectList.tsx
@@ -1,25 +1,9 @@
-import { Link } from 'react-router-dom'
 import { ProjectCard } from '../components/ProjectCard'
 import projects from '../utils/projects.json'
-import { House } from 'lucide-react';
 
 export const ProjectList = () => {
   return (
     <>
-      <header>
-        <div id="logo">
-          <img src="/logo_png.png" alt="" />
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>
-                  <House />
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <main>
         <section id='projects-container' className='container'>
           {projects.map(project => <ProjectCard key={project.id} project={project} />)}
