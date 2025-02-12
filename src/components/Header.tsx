3a235aee420e68dcdfa97d8ec9889483bac7cd30
Athina <athina.kantis@gmail.com>
2025-02-12 20:08:27 +0200

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Nav } from './Nav'
+
+export const Header = () => {
+  return (
+    <header>
+      <div id="logo">
+        <img src="/logo_png.png" alt="" />
+      </div>
+      <Nav />
+    </header>
+  )
+}
