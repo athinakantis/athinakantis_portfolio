25151d51a3895d1c94f86de8807df8fa8d1d71f9
Athina <athina.kantis@gmail.com>
2025-02-12 20:07:30 +0200

    add gallery data

---
+++ src/utils/gallery.ts
@@ -0,0 +1,25 @@
+export const art = {
+  portraits: [
+    "portrait_1.png",
+    "portrait_2.png",
+    "portrait_3.png",
+    "portrait_4.png",
+    "portrait_5.png"
+  ],
+  landscapes: [
+    "landscape_1.png",
+    "landscape_2.png",
+    "landscape_3.png",
+    "landscape_4.png",
+    "landscape_5.png",
+    "landscape_6.png",
+    "landscape_7.png"
+  ],
+  illustrations: [
+    "illustration_1.png",
+    "illustration_2.png",
+    "illustration_3.png",
+    "illustration_4.png",
+    "illustration_5.png"
+  ]
+}
