c9fe450336c39c317d4fe0284cacf98c932267a1
Athina <athina.kantis@gmail.com>
2025-02-12 20:09:52 +0200

    update LandingPage to only animate logo on first visit

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,59 +1,80 @@
 import { Link } from 'react-router-dom';
 import { Palette, CircleUserRound, BriefcaseBusiness, Github, Linkedin } from 'lucide-react';
+import { AnimatedLogo } from '../components/AnimatedLogo';
+import { motion } from 'motion/react';
+import { useEffect, useState } from 'react';
 
 
 function LandingPage() {
+  const [isFirstVisit, setIsFirstVisit] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState(true)
+
+  // Check if it's users first visit
+  useEffect(() => {
+    const visit = sessionStorage.getItem("firstVisit");
+    if (!visit) {
+      setIsFirstVisit(true)
+      sessionStorage.setItem('firstVisit', 'true')
+    }
+    setIsLoading(false)
+  }, []);
+
+  if (isLoading) {
+    return (<></>)
+  }
+
   return (
     <>
       <main>
         <div id="landing-page">
 
-          <div id="logo">
-            <img src="/logo_png.png" alt="" />
-          </div>
-          <p>Hello :) I'm Athina, a Full-Stack Web Developer, Art Graduate and ex-Nurse Assistant.</p>
+          <AnimatedLogo isFirstVisit={isFirstVisit} />
 
-          <nav>
-            <ul>
-              <li>
-                <Link to='/'></Link>
-              </li>
-              <li>
-                <Link to='/projects'>
-                  <BriefcaseBusiness />
-                  Projects</Link>
-              </li>
-              <li>
-                <Link to='/about'>
-                <CircleUserRound />
-                About
-                </Link>
-              </li>
-              <li>
-                <Link to=''>
-                <Palette />
-                Art
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <motion.div className="content" initial={isFirstVisit ? { opacity: 0 } : { opacity: 1 }} animate={isFirstVisit ? { opacity: 1, transition: { delay: 3, duration: 1.2, ease: 'easeOut' } } : {}}>
+            <p>Hello :) I'm Athina, a Full-Stack Web Developer, Art Graduate and ex-Nurse Assistant.</p>
 
-          <div id="socials">
-            <p>Let's Connect!</p>
-            <ul>
-              <li>
-                <Link target='_blank' to='https://github.com/athinakantis'>
-                  <Github />
-                </Link>
-              </li>
-              <li>
+            <nav>
+              <ul>
+                <li>
+                  <Link to='/'></Link>
+                </li>
+                <li>
+                  <Link to='/home/projects'>
+                    <BriefcaseBusiness />
+                    Projects</Link>
+                </li>
+                <li>
+                  <Link to='/home/about'>
+                    <CircleUserRound />
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/home/gallery'>
+                    <Palette />
+                    Art
+                  </Link>
+                </li>
+              </ul>
+            </nav>
 
-                <Link target='_blank' to='https://www.linkedin.com/in/athina-kantis'>
-                  <Linkedin />
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <div id="socials">
+              <p>Let's Connect!</p>
+              <ul>
+                <li>
+                  <Link target='_blank' to='https://github.com/athinakantis'>
+                    <Github />
+                  </Link>
+                </li>
+                <li>
+
+                  <Link target='_blank' to='https://www.linkedin.com/in/athina-kantis'>
+                    <Linkedin />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </motion.div>
         </div>
       </main>
     </>
