f8c31633652165b01270b419781b5e3544dddf9c
Athina <athina.kantis@gmail.com>
2025-02-16 21:54:37 +0200

    remove unecessary animation

--- src/components/GalleryCategory.tsx
+++ src/components/GalleryCategory.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from 'react';
-import { animate, motion, type MotionValue, useMotionValue, useMotionValueEvent, useScroll } from "motion/react";
+import { useRef } from 'react';
+import { motion } from "motion/react";
 import { art } from '../utils/data';
 import { ArtCategories } from '../pages/Gallery';
 
@@ -8,51 +8,11 @@ interface GalleryCategoryProps {
 }
 
 export const GalleryCategory = ({ category }: GalleryCategoryProps) => {
-  const top = `0%`
-  const bottom = `100%`
-  const topInset = `20%`
-  const bottomInset = `80%`
-  const transparent = `#0000`
-  const opaque = `#000`
   const ref = useRef(null)
-  const { scrollYProgress } = useScroll({ container: ref })
-  const maskImage = useScrollOverflowMask(scrollYProgress)
 
-
-
-  function useScrollOverflowMask(scrollYProgress: MotionValue<number>) {
-    const maskImage = useMotionValue(
-      `linear-gradient(0deg, #000, #000 100%, #000 76%, #0000)`,
-    )
-    // Gradient
-    // linear-gradient(0deg, #000, #000 86%, #000 76%, #0000) 
-
-    // No Gradient
-    // linear-gradient(0deg, #000, #000 100%, #000 76%, #0000);
-
-    useMotionValueEvent(scrollYProgress, "change", (value) => {
-      if (value > 0) {
-        animate(
-          maskImage,
-          `linear-gradient(0deg, #000, #000 86%, #000 76%, #0000)`,
-        )
-      } else {
-        animate(
-          maskImage,
-          `linear-gradient(0deg, #000, #000 100%, #000 76%, #0000)`
-        )
-      }
-
-    })
-    return maskImage
-  }
-
-  useEffect(() => {
-    console.log(scrollYProgress)
-  }, [scrollYProgress])
 
   return (
-    <div style={{ maskImage: maskImage.get(), overflow: 'scrollY' }} ref={ref} key={category} className={`${category} category`}>
+    <div ref={ref} key={category} className={`${category} category`}>
       <h2>{category}</h2>
       {(art[category as ArtCategories] || []).map(item =>
         <motion.img key={item} src={`/art/${item}`}
