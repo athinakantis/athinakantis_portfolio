f74490e1affd90656067dce7abea9f7a5f9733cf
Athina <athina.kantis@gmail.com>
2025-02-17 17:38:51 +0200

    add request to projects storage

--- src/pages/ProjectList.tsx
+++ src/pages/ProjectList.tsx
@@ -1,25 +1,51 @@
+import { useEffect, useState } from 'react'
 import { ProjectCard } from '../components/ProjectCard'
 import { techStack } from '../utils/data'
-import projects from '../utils/projects.json'
+import { ProjectT } from '../types/types'
+import { Spinner } from '../components/Spinner'
+import { motion } from 'motion/react'
+
 
 export const ProjectList = () => {
+  const [projects, setProjects] = useState<null | ProjectT[]>(null)
+  const [loading, setLoading] = useState(false)
+
+  async function fetchProjects() {
+    try {
+      setLoading(true)
+      const response = await fetch(`${import.meta.env.VITE_SUPABASE_STORAGE}/projects.json`)
+      const data = await response.json()
+      setProjects(data)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchProjects()
+  }, [])
+
+  if (loading) return <Spinner />
+
   return (
-    <>
-      <section id='projects-container' className='container'>
-        {projects.map(project => <ProjectCard key={project.id} project={project} />)}
-      </section>
+    <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} id='projects-container' className='container'>
+      <div id='project-cards-container'>
+
+        {projects?.map(project => <ProjectCard key={project.id} project={project} />)}
+      </div>
       <div id="tech-stack-wrapper">
         <h2>Tech Stack</h2>
         <div id="tech-stack-container">
           {techStack.map(tech => (
-            <div className='tech-container'>
-              <img src={`/tech_stack/${tech.toLowerCase()}.png`} alt='' />
+            <div key={tech} className='tech-container'>
+              <img src={`/tech_stack/${tech.toLowerCase()}.svg`} alt='' />
               <span className='tech-span'>{tech}</span>
             </div>
           ))}
         </div>
       </div>
-    </>
-
+    </motion.section>
   )
 }
