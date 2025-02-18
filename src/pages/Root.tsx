7cfd62243bdbee5fb6553384a8493025e9cc9839
Athina <athina.kantis@gmail.com>
2025-02-18 08:51:59 +0200

    ensure users cannot navigate to /home, redirects to projects

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,7 +1,13 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { Header } from '../components/Header'
 
 export const Root = () => {
+  const location = useLocation();
+  const regex = new RegExp('^/?home/?$')
+
+  if (location.pathname.match(regex)) {
+    return <Navigate to='/home/projects' />
+  }
 
   return (
     <>
