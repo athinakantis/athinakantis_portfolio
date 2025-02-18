f9343d7bee9596cd60ed99dd1fe5e2bef4406e90
Athina <athina.kantis@gmail.com>
2025-02-18 12:16:52 +0200

    remove redundant animations

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,17 +1,37 @@
-import { motion } from 'motion/react';
 import { GalleryCategory } from '../components/GalleryCategory';
 import { art } from '../utils/data';
+import { Spinner } from '../components/Spinner';
+import { useEffect, useState } from 'react';
 
 export type ArtCategories = 'portraits' | 'landscapes' | 'illustrations';
 
 export const Gallery = () => {
+  const totalImages = Object.values(art).flat().length;
+  const [loadedImages, setLoadedImages] = useState(0);
+  const [allLoaded, setAllLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setLoadedImages((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedImages === totalImages) setAllLoaded(true);
+  }, [loadedImages, totalImages]);
 
   return (
-    <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} id="gallery-container" className='container'>
-      {Object.keys(art).map(category => (
-        <GalleryCategory key={category} category={category as ArtCategories} />
+    <section
+      id='gallery-container'
+      className='container'
+    >
+      {!allLoaded && <Spinner type='Art' />}
+      {Object.keys(art).map((category) => (
+        <GalleryCategory
+          key={category}
+          category={category as ArtCategories}
+          onImageLoad={handleImageLoad}
+          allLoaded={allLoaded}
+        />
       ))}
-    </motion.section>
-  )
-
-}
+    </section>
+  );
+};
